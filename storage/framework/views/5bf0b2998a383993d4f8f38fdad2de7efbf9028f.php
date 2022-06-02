6d3ebecbfce6d78639560eb744eb5c46558ce988
unknown <pukna_9009@hotmail.com>
2022-06-02 12:24:07 -0500

    #2 Depuracion Front Registro

--- storage/framework/views/5bf0b2998a383993d4f8f38fdad2de7efbf9028f.php
+++ storage/framework/views/5bf0b2998a383993d4f8f38fdad2de7efbf9028f.php
@@ -4,61 +4,62 @@
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title><?php echo e($general_setting->site_title); ?></title>
+      <link rel="icon" type="image/png" href="<?php echo asset('images/login/LogoTactotal.png') ?>" />
+      <title>SISTEMA TECTOTAL</title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="robots" content="all,follow">
     <!-- Bootstrap CSS-->
-    <link rel="stylesheet" href="<?php echo asset('public/vendor/bootstrap/css/bootstrap.min.css') ?>" type="text/css">
-    <link rel="stylesheet" href="<?php echo asset('public/vendor/bootstrap/css/bootstrap-datepicker.min.css') ?>" type="text/css">
-    <link rel="stylesheet" href="<?php echo asset('public/vendor/bootstrap/css/bootstrap-select.min.css') ?>" type="text/css">
+    <link rel="stylesheet" href="<?php echo asset('vendor/bootstrap/css/bootstrap.min.css') ?>" type="text/css">
+    <link rel="stylesheet" href="<?php echo asset('vendor/bootstrap/css/bootstrap-datepicker.min.css') ?>" type="text/css">
+    <link rel="stylesheet" href="<?php echo asset('vendor/bootstrap/css/bootstrap-select.min.css') ?>" type="text/css">
     <!-- Font Awesome CSS-->
-    <link rel="stylesheet" href="<?php echo asset('public/vendor/font-awesome/css/font-awesome.min.css') ?>" type="text/css">
+    <link rel="stylesheet" href="<?php echo asset('vendor/font-awesome/css/font-awesome.min.css') ?>" type="text/css">
     <!-- Fontastic Custom icon font-->
-    <link rel="stylesheet" href="<?php echo asset('public/css/fontastic.css') ?>" type="text/css">
+    <link rel="stylesheet" href="<?php echo asset('css/fontastic.css') ?>" type="text/css">
     <!-- Google fonts - Roboto -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700">
     <!-- jQuery Circle-->
-    <link rel="stylesheet" href="<?php echo asset('public/css/grasp_mobile_progress_circle-1.0.0.min.css') ?>" type="text/css">
+    <link rel="stylesheet" href="<?php echo asset('css/grasp_mobile_progress_circle-1.0.0.min.css') ?>" type="text/css">
     <!-- Custom Scrollbar-->
-    <link rel="stylesheet" href="<?php echo asset('public/vendor/malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar.css') ?>" type="text/css">
+    <link rel="stylesheet" href="<?php echo asset('vendor/malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar.css') ?>" type="text/css">
     <!-- theme stylesheet-->
-    <link rel="stylesheet" href="<?php echo asset('public/css/style.default.css') ?>" id="theme-stylesheet" type="text/css">
+    <link rel="stylesheet" href="<?php echo asset('css/style.default.css') ?>" id="theme-stylesheet" type="text/css">
     <!-- Custom stylesheet - for your changes-->
-    <link rel="stylesheet" href="<?php echo asset('public/css/custom-'.$general_setting->theme) ?>" type="text/css">
+    <link rel="stylesheet" href="<?php echo asset('css/custom-'.$general_setting->theme) ?>" type="text/css">
     <!-- Favicon-->
-    <link rel="shortcut icon" href="img/favicon.ico">
+<!--    <link rel="shortcut icon" href="img/favicon.ico">-->
     <!-- Tweaks for older IEs--><!--[if lt IE 9]>
         <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
         <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script><![endif]-->
 
-    <script type="text/javascript" src="<?php echo asset('public/vendor/jquery/jquery.min.js') ?>"></script>
-<script type="text/javascript" src="<?php echo asset('public/vendor/jquery/jquery-ui.min.js') ?>"></script>
-<script type="text/javascript" src="<?php echo asset('public/vendor/jquery/bootstrap-datepicker.min.js') ?>"></script>
-<script type="text/javascript" src="<?php echo asset('public/vendor/popper.js/umd/popper.min.js') ?>">
+    <script type="text/javascript" src="<?php echo asset('vendor/jquery/jquery.min.js') ?>"></script>
+<script type="text/javascript" src="<?php echo asset('vendor/jquery/jquery-ui.min.js') ?>"></script>
+<script type="text/javascript" src="<?php echo asset('vendor/jquery/bootstrap-datepicker.min.js') ?>"></script>
+<script type="text/javascript" src="<?php echo asset('vendor/popper.js/umd/popper.min.js') ?>">
 </script>
-<script type="text/javascript" src="<?php echo asset('public/vendor/bootstrap/js/bootstrap.min.js') ?>"></script>
-<script type="text/javascript" src="<?php echo asset('public/vendor/bootstrap/js/bootstrap-select.min.js') ?>"></script>
-<script type="text/javascript" src="<?php echo asset('public/js/grasp_mobile_progress_circle-1.0.0.min.js') ?>"></script>
-<script type="text/javascript" src="<?php echo asset('public/vendor/jquery.cookie/jquery.cookie.js') ?>">
+<script type="text/javascript" src="<?php echo asset('vendor/bootstrap/js/bootstrap.min.js') ?>"></script>
+<script type="text/javascript" src="<?php echo asset('vendor/bootstrap/js/bootstrap-select.min.js') ?>"></script>
+<script type="text/javascript" src="<?php echo asset('js/grasp_mobile_progress_circle-1.0.0.min.js') ?>"></script>
+<script type="text/javascript" src="<?php echo asset('vendor/jquery.cookie/jquery.cookie.js') ?>">
 </script>
-<script type="text/javascript" src="<?php echo asset('public/vendor/chart.js/Chart.min.js') ?>"></script>
-<script type="text/javascript" src="<?php echo asset('public/vendor/jquery-validation/jquery.validate.min.js') ?>"></script>
-<script type="text/javascript" src="<?php echo asset('public/vendor/malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar.concat.min.js')?>"></script>
-<script type="text/javascript" src="<?php echo asset('public/js/charts-home.js') ?>"></script>
-<script type="text/javascript" src="<?php echo asset('public/js/front.js') ?>"></script>
+<script type="text/javascript" src="<?php echo asset('vendor/chart.js/Chart.min.js') ?>"></script>
+<script type="text/javascript" src="<?php echo asset('vendor/jquery-validation/jquery.validate.min.js') ?>"></script>
+<script type="text/javascript" src="<?php echo asset('vendor/malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar.concat.min.js')?>"></script>
+<script type="text/javascript" src="<?php echo asset('js/charts-home.js') ?>"></script>
+<script type="text/javascript" src="<?php echo asset('js/front.js') ?>"></script>
   </head>
   <body>
     <div class="page login-page">
       <div class="container">
         <div class="form-outer text-center d-flex align-items-center">
           <div class="form-inner">
-            <div class="logo"><spanS>Tectotal</span></div>
+            <div class="logo"><span>Tectotal</span></div>
             <form method="POST" action="<?php echo e(route('register')); ?>">
                 <?php echo csrf_field(); ?>
               <div class="form-group-material">
                 <input id="register-username" type="text" name="name" required class="input-material">
-                <label for="register-username" class="label-material"><?php echo e(trans('file.Correo')); ?> *</label>
+                <label for="register-username" class="label-material"><?php echo e(trans('Correo')); ?> *</label>
                 <?php if($errors->has('name')): ?>
                     <p>
                         <strong><?php echo e($errors->first('name')); ?></strong>
@@ -67,7 +68,7 @@
               </div>
               <div class="form-group-material">
                 <input id="register-email" type="email" name="email" required class="input-material">
-                <label for="register-email" class="label-material"><?php echo e(trans('file.Confirmar Email')); ?> *</label>
+                <label for="register-email" class="label-material"><?php echo e(trans('Confirmar Email')); ?> *</label>
                 <?php if($errors->has('email')): ?>
                     <p>
                         <strong><?php echo e($errors->first('email')); ?></strong>
@@ -76,11 +77,11 @@
               </div>
               <div class="form-group-material">
                 <input id="register-phone" type="text" name="phone" required class="input-material">
-                <label for="register-phone" class="label-material"><?php echo e(trans('file.Numero de telefono')); ?> *</label>
+                <label for="register-phone" class="label-material"><?php echo e(trans('Numero de telefono')); ?> *</label>
               </div>
               <div class="form-group-material">
                 <input id="register-company" type="text" name="company_name" class="input-material">
-                <label for="register-company" class="label-material"><?php echo e(trans('file.Cargo')); ?></label>
+                <label for="register-company" class="label-material"><?php echo e(trans('Cargo')); ?></label>
               </div>
               <div class="form-group-material">
                 <select name="role_id" required class="selectpicker form-control" data-live-search="true" data-live-search-style="begins" title="Select Role...">
@@ -114,10 +115,10 @@
               </div>
               <div class="form-group-material">
                 <input id="password-confirm" type="password" name="password_confirmation" required class="input-material">
-                <label for="password-confirm" class="label-material"><?php echo e(trans('file.Repita Password')); ?> *</label>
+                <label for="password-confirm" class="label-material"><?php echo e(trans('Repita Password')); ?> *</label>
               </div>
-              <input id="register" type="submit" value="Register" class="btn btn-primary">
-            </form><p><?php echo e(trans('file.Already have an account')); ?>? </p><a href="<?php echo e(url('login')); ?>" class="signup"><?php echo e(trans('file.LogIn')); ?></a>
+              <input id="register" type="submit" value="Registrar" class="btn btn-primary">
+            </form><p><?php echo e(trans('¿Ya tienes una cuenta?')); ?>? </p><a href="<?php echo e(url('login')); ?>" class="signup"><?php echo e(trans('file.LogIn')); ?></a>
           </div>
         </div>
       </div>
