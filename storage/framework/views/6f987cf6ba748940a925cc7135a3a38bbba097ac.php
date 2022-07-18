d470e08748404ad5bcf1128601ef151ef982a062
unknown <pukna_9009@hotmail.com>
2022-07-18 12:49:22 -0500

    #19 Front Agregar CLiente

---
+++ storage/framework/views/6f987cf6ba748940a925cc7135a3a38bbba097ac.php
@@ -0,0 +1,113 @@
+ <?php $__env->startSection('content'); ?>
+<?php if(session()->has('not_permitted')): ?>
+  <div class="alert alert-danger alert-dismissible text-center"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button><?php echo e(session()->get('not_permitted')); ?></div>
+<?php endif; ?>
+<section class="forms">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header d-flex align-items-center">
+                        <h4><?php echo e(trans('Add Clienter')); ?></h4>
+                    </div>
+                    <div class="card-body">
+                        <p class="italic"><small><?php echo e(trans('Las etiquetas de campo marcadas con * son campos de entrada obligatorios')); ?>.</small></p>
+                        <?php echo Form::open(['route' => 'customer.store', 'method' => 'post', 'files' => true]); ?>
+
+                        <div class="row">
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <label><?php echo e(trans('Grupo de cliente')); ?> *</strong> </label>
+                                    <select required class="form-control selectpicker" name="customer_group_id">
+                                        <?php $__currentLoopData = $lims_customer_group_all; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $customer_group): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                                            <option value="<?php echo e($customer_group->id); ?>"><?php echo e($customer_group->name); ?></option>
+                                        <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <label><?php echo e(trans('Nombre')); ?> *</strong> </label>
+                                    <input type="text" name="name" required class="form-control">
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <label><?php echo e(trans('nombre de Empresa')); ?></label>
+                                    <input type="text" name="company_name" class="form-control">
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <label><?php echo e(trans('file.Email')); ?></label>
+                                    <input type="email" name="email" placeholder="example@example.com" class="form-control">
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <label><?php echo e(trans('Numero de telefono')); ?> *</label>
+                                    <input type="text" name="phone_number" required class="form-control">
+                                    <?php if($errors->has('phone_number')): ?>
+                                   <span>
+                                       <strong><?php echo e($errors->first('phone_number')); ?></strong>
+                                    </span>
+                                    <?php endif; ?>
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <label><?php echo e(trans('Ruc')); ?></label>
+                                    <input type="text" name="tax_no" class="form-control">
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <label><?php echo e(trans('Dirección')); ?> *</label>
+                                    <input type="text" name="address" required class="form-control">
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <label><?php echo e(trans('Ciodad')); ?> *</label>
+                                    <input type="text" name="city" required class="form-control">
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <label><?php echo e(trans('Provincia')); ?></label>
+                                    <input type="text" name="state" class="form-control">
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <label><?php echo e(trans('Codigo Postal')); ?></label>
+                                    <input type="text" name="postal_code" class="form-control">
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <label><?php echo e(trans('Pais')); ?></label>
+                                    <input type="text" name="country" class="form-control">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <input type="hidden" name="pos" value="0">
+                            <input type="submit" value="<?php echo e(trans('Enviar')); ?>" class="btn btn-primary">
+                        </div>
+                        <?php echo Form::close(); ?>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<script type="text/javascript">
+    $("ul#people").siblings('a').attr('aria-expanded','true');
+    $("ul#people").addClass("show");
+    $("ul#people #customer-create-menu").addClass("active");
+</script>
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('layout.main', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Users\Mario Montero\ERP_contable_laravel\resources\views/customer/create.blade.php ENDPATH**/ ?>
