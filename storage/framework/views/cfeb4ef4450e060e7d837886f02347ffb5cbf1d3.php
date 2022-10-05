dc80f381b4e4a13798d11e64071473e725bf2c44
unknown <pukna_9009@hotmail.com>
2022-10-05 16:49:11 -0500

    #63 Cambios en agregar y actuañizar provvedor

--- storage/framework/views/cfeb4ef4450e060e7d837886f02347ffb5cbf1d3.php
+++ storage/framework/views/cfeb4ef4450e060e7d837886f02347ffb5cbf1d3.php
@@ -1,7 +1,7 @@
 <!--agregar proveedor -->
 <?php $__env->startSection('content'); ?>
 <?php if(session()->has('not_permitted')): ?>
-  <div class="alert alert-danger alert-dismissible text-center"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button><?php echo e(session()->get('not_permitted')); ?></div>
+  <div class="alert alert-danger alert-dismissible text-center" xmlns="http://www.w3.org/1999/html"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button><?php echo e(session()->get('not_permitted')); ?></div>
 <?php endif; ?>
 <section class="forms">
     <div class="container-fluid">
@@ -17,7 +17,7 @@
 
                         <div class="row">
                             <div class="col-md-12 mt-3">
-                                <label><h5> <?php echo e(trans('Datos Empresa')); ?></h5></label>
+                                <label><h5> <?php echo e(trans('Información Proveedor - Tributaria')); ?></h5></label>
                                 <hr/>
                             </div>
 <!--                            <div class="col-md-6">-->
@@ -62,53 +62,28 @@
                             </div>
                             <div id="ruc" class="col-md-6">
                                 <div class="form-group">
-                                    <label><?php echo e(trans('Ruc')); ?></label>
-                                    <input type="text" name="type_ruc"  placeholder="1111111111001" class="form-control ">
+                                    <label><?php echo e(trans('Ruc')); ?>*</label>
+                                    <input type="text" name="vat_number"  placeholder="1111111111001" class="form-control ">
                                 </div>
                             </div>
                             <div id="identity_card" class="col-md-6">
                                 <div class="form-group">
-                                    <label><?php echo e(trans('Cedula')); ?></label>
+                                    <label><?php echo e(trans('Cedula')); ?>*</label>
                                     <input type="text" name="type_id"  placeholder="1111111111" class="form-control ">
                                 </div>
                             </div>
                             <div id="passport" class="col-md-6">
                                 <div class="form-group">
-                                    <label><?php echo e(trans('Pasaporte')); ?></label>
+                                    <label><?php echo e(trans('Pasaporte')); ?>*</label>
                                     <input type="text" name="type_passport"  placeholder="A1111111111001A" class="form-control ">
                                 </div>
-                            </div>
-<!--                            <div id="ruc1" class="col-md-6">-->
-<!--                                <div class="form-group">-->
-<!--                                </div>-->
-<!--                            </div>-->
-<!--                            <div id="ruc1" class="col-md-6">-->
-<!--                                <div class="form-group">-->
-<!--                                    <input type="text" name="type_ruc"  placeholder="1111111111001" class="form-control ">-->
-<!--                                </div>-->
-<!--                            </div>-->
-<!--                            <div id="identity_card" class="col-md-6">-->
-<!--                                <div class="form-group">-->
-<!--                                    <label>--><?php //echo e(trans('Cedula')); ?><!--*</strong></label>-->
-<!--                                    <input type="text" name="type_ced" placeholder="1111111111"  class="form-control">-->
-<!--                                </div>-->
-<!--                            </div>-->
-<!--                            <div id="passport" class="col-md-6">-->
-<!--                                <div class="form-group">-->
-<!--                                    <label>--><?php //echo e(trans('Pasaporte')); ?><!--*</strong></label>-->
-<!--                                    <input type="text" name="" placeholder="A1111111111A"  class="form-control">-->
-<!--                                </div>-->
-<!--                            </div>-->
-                            <div class="col-md-12 mt-3">
-                                <label><h5> <?php echo e(trans('Datos de Contacto')); ?></h5></label>
-                                <hr/>
                             </div>
                             <div class="col-md-6">
                                 <div class="form-group">
                                     <label><?php echo e(trans('file.Email')); ?> *</label>
                                     <input type="email" name="email" placeholder="example@example.com" required class="form-control">
                                     <?php if($errors->has('email')): ?>
-                                   <span>
+                                        <span>
                                        <strong><?php echo e($errors->first('email')); ?></strong>
                                     </span>
                                     <?php endif; ?>
@@ -118,6 +93,22 @@
                                 <div class="form-group">
                                     <label><?php echo e(trans('Numero de telefono')); ?> *</label>
                                     <input type="text" name="phone_number" required class="form-control">
+                                </div>
+                            </div>
+                            <div class="col-md-12 mt-3">
+                                <label><h5> <?php echo e(trans('Datos de Contacto')); ?></h5></label>
+                                <hr/>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <label><?php echo e(trans('Nombre Contacto')); ?> *</label>
+                                    <input type="text" name="contact_name" required class="form-control">
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <label><?php echo e(trans('Email Contacto')); ?> *</label>
+                                    <input type="email" name="contact_email" placeholder="example@example.com" required class="form-control">
                                 </div>
                             </div>
                             <div class="col-md-12 mt-3">
@@ -132,8 +123,8 @@
                             </div>
                             <div class="col-md-6">
                                 <div class="form-group">
-                                    <label><?php echo e(trans('Ciudad')); ?> *</label>
-                                    <input type="text" name="city" required class="form-control">
+                                    <label><?php echo e(trans('Ciudad')); ?></label>
+                                    <input type="text" name="city" class="form-control">
                                 </div>
                             </div>
                             <div class="col-md-6">
@@ -148,12 +139,42 @@
                                     <input type="text" name="postal_code" class="form-control">
                                 </div>
                             </div>
-<!--                            <div class="col-md-6">-->
-<!--                                <div class="form-group">-->
-<!--                                    <label>--><?php //echo e(trans('Pais')); ?><!--</label>-->
-<!--                                    <input type="text" name="country" class="form-control">-->
-<!--                                </div>-->
-<!--                            </div>-->
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <label><?php echo e(trans('Pais')); ?></label>
+                                    <input type="text" name="country" class="form-control">
+                                </div>
+                            </div>
+                            <div class="col-md-4">
+                                <div class="form-group mt-3">
+                                    <input type="checkbox" name="special_taxpayer" value="1">&nbsp;
+                                    <label><?php echo e(trans('Contribuyente Especial')); ?></label>
+<!--                                    <p class="italic">--><?php //echo e(trans('El producto destacado se mostrará en POS')); ?><!--</p>-->
+                                </div>
+                            </div>
+                            <div class="col-md-12 mt-3">
+                                <label><h5> <?php echo e(trans('Credito')); ?></h5></label>
+                                <hr/>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <label><?php echo e(trans('Metodo de Pago')); ?>*</label>
+                                    <div class="input-group">
+                                        <select name="way_payment" required class="form-control selectpicker" id="type">
+                                            <option value="credit">Crédito</option>
+                                            <option value="cash">Contado</option>
+                                        </select>
+                                    </div>
+                                </div>
+                            </div>
+                            <div id="credit" class="col-md-4">
+                                <div class="form-group">
+                                    <label><?php echo e(trans('Plazo')); ?>*</label>
+                                    <input type="text" name="payment_deadline"  placeholder="Ingresar valores en días" class="form-control ">
+                                </div>
+                            </div>
+
+
                             <div class="col-md-12 mt-3">
                                 <label><h5> <?php echo e(trans('Datos Bancarios')); ?></h5></label>
                                 <hr/>
@@ -170,7 +191,7 @@
 <!--                                        </div>-->
                                         <div class="col-md-4">
                                             <div class="form-group">
-                                                <label><?php echo e(trans('Banco')); ?></strong> </label>
+                                                <label><?php echo e(trans('Banco')); ?>*</strong> </label>
                                                 <div class="input-group">
                                                     <select name="bank_id" class="selectpicker form-control" data-live-search="true" data-live-search-style="begins" title="Seleccionar banco...">
                                                         <?php $__currentLoopData = $lims_bank_list; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $bank): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
@@ -226,6 +247,7 @@
 
     $("#identity_card").hide();
     $("#passport").hide();
+    $("#payment_deadline").hide();
     var ruc = $('input[name="type_ruc"]').val();
     var id = $('input[name="type_id"]').val();
 
@@ -255,10 +277,25 @@
 
         }
     });
+
+    // select credit
+
+    $('select[name="way_payment"]').on('change', function() {
+        if($(this).val() == 'credit'){
+            hide();
+            $("#credit").show(300);
+            $("input[name='payment_deadline']").prop('required',true);
+        }
+        else if($(this).val() == 'cash'){
+            hide();
+            $("#credit").hide(300);
+        }
+    });
     function hide() {
         $("#ruc").hide(300);
         $("#pasaport").hide(300);
         $("#identification_card").hide(300);
+        $("#payment_deadline").hide(300);
     }
 </script>
 <?php $__env->stopSection(); ?>
